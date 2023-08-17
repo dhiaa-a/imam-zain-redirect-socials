183e7efa5777597d9fbc860f4f8414fec8a23746
dhiaa <dhiaajhamed@gmail.com>
2023-08-17 13:19:31 +0300

    init commit

---
+++ netlify/functions/redirect.js
@@ -0,0 +1,29 @@
+const CURRENT_DESTINATION_URL = "http://www.example1.com";
+
+exports.handler = async (event, context) => {
+    try {
+        // Validate HTTP method
+        if (event.httpMethod !== 'GET') {
+            return { statusCode: 405, body: 'Method Not Allowed' };
+        }
+
+        // Return the redirection response with security headers
+        return {
+            statusCode: 302,
+            headers: {
+                'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+                'X-Content-Type-Options': 'nosniff',
+                'X-Frame-Options': 'DENY',
+                'X-XSS-Protection': '1; mode=block',
+                'Referrer-Policy': 'strict-origin-when-cross-origin',
+                Location: CURRENT_DESTINATION_URL
+            }
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: 'Internal Server Error'
+        };
+    }
+};
